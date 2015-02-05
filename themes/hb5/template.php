9a78565d45455af90f81753fad0a25d8f24190a1
Bayo Fodeke <bfodeke@gmail.com>
2015-02-05 13:24:39 -0500

    Added google analytics code

--- themes/hb5/template.php
+++ themes/hb5/template.php
@@ -88,6 +88,17 @@ function hb5_preprocess_page(&$variables) {
     ),
   );
   drupal_add_html_head($header_bar_theme_color, 'header_bar_theme_color');
+
+  // Add Google Analytics code
+  $google_analytics = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-59396163-1', 'auto');
+  ga('send', 'pageview');";
+
+  drupal_add_js ($google_analytics,array('type' => 'inline','scope' => 'header','weight' =>5));
 }
 
 /**
